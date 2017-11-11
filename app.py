05339546a4633010fa8a5dadab1d18b3fdc6f37e
Vidya Venkiteswaran <vidyavenkiteswaran@nat-tvwna-inside-visitornet101-b-739.princeton.org>
2017-11-11 18:14:01 -0500

    Add blob url

--- app.py
+++ app.py
@@ -71,7 +71,8 @@ def upload_file():
             filename = secure_filename(file.filename)
             block_blob_service.create_blob_from_stream(container, filename, file)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            result = VIDEOS_COLLECTION.update({'video_name': request.form['name']+'.mp4', 'video_url': request.form['name']+'.mp4', 'video_desc': request.form['desc'], 'video_lang': request.form['lang']}, 
+            blob_url = 'https://instatranslatefile.blob.core.windows.net/resources/'
+            result = VIDEOS_COLLECTION.update({'video_name': request.form['name']+'.mp4', 'video_url': blob_url+request.form['name']+'.mp4', 'video_desc': request.form['desc'], 'video_lang': request.form['lang']}, 
                 {'video_name': request.form['name']+'.mp4', 'video_url': request.form['name']+'.mp4', 'video_desc': request.form['desc'], 'video_lang': request.form['lang']}, upsert=True)
             return redirect(url_for('index'))
     return "Upload Fail"
