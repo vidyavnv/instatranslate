c07e46e267e10c86761bdc0ca3ef242d031d2021
jamshed994 <jamshed994@gmail.com>
2017-11-11 21:04:47 -0500

    Front End changes

--- Angular/angular_app.js
+++ Angular/angular_app.js
@@ -2,69 +2,79 @@ angular.module('MyApp', ['angularUtils.directives.dirPagination','oitozero.ngSwe
    .controller('MainController', ['$scope','$http','SweetAlert','$location',function($scope,$http,SweetAlert,$location) { 
 
 
-      $scope.greeting = "Angular Scope Connected";
-      $scope.show=true;
-      //SweetAlert.swal("Success!", "User details have been Stored! ", "success");
-      console.log("Hello");
-      var APIUrl="http://localhost:5000";
+    $scope.greeting = "Angular Scope Connected";
+    $scope.show=true;
+    $scope.selectedVideo="hello";
+    //SweetAlert.swal("Success!", "User details have been Stored! ", "success");
+    console.log("Hello");
+    var APIUrl="http://localhost:5000";
 
 
-      $scope.languages=["English","French","German"];
-      //Code to upload Video
-      $scope.upload_video = function() {
-          console.log($scope.video);
-      		var res = $http.post(APIUrl+'/uploadFile',$scope.video);
-    			res.success(function(data, status, headers, config) {
-    				$scope.message = data;
-    			});
-    			res.error(function(data, status, headers, config) {
-    				alert( "failure message: " + JSON.stringify({data: data}));
-  			  });
-          SweetAlert.swal("Success!", "Video has been uploaded! ", "success");
-      }
+	$scope.languages=["English","French","German"];
+	
+	//Code to upload Video
+  	$scope.upload_video = function() {
+      	console.log($scope.video);
+  		var res = $http.post(APIUrl+'/uploadFile',$scope.video);
+			res.success(function(data, status, headers, config) {
+				$scope.message = data;
+			});
+			res.error(function(data, status, headers, config) {
+				alert( "failure message: " + JSON.stringify({data: data}));
+			  });
+     	SweetAlert.swal("Success!", "Video has been uploaded! ", "success");
+  	}
 
-      $scope.get_all_videos=function(){
+    $scope.get_all_videos=function(){
         $http({
               method: 'GET',
               url: APIUrl+"/getVideos",
               headers: {
                   'Content-type': 'application/json;'
               }
-          })
-          .then(function(response) {
+        })
+        .then(function(response) {
           	console.log(response.data);
               $scope.videos = response.data;
-              // console.log("");
               console.log($scope.videos);
-          }, 
-          function(rejection) {
+        }, 
+        function(rejection) {
               console.log( "failure message: " + JSON.stringify({data: data}));
+        });
+    }
+
+    //Function for send request for translating videos
+    $scope.translatevideo=function(selectedVideo){
+          console.log($scope.selectedVideo);
+          var res = $http.post(APIUrl+'/gettranslationreq',JSON.stringify($scope.selectedVideo));
+          res.success(function(data, status, headers, config) {
+            $scope.message = data;
           });
-      }
+          res.error(function(data, status, headers, config) {
+            alert( "failure message: " + JSON.stringify({data: data}));
+          });
+          SweetAlert.swal("Success!", "Translation Request Submitted, video will be available soon! ", "success");
+    }
 
 
-      $scope.redirect = function(){
-	  	$location.url('/convertvideo.html');
-		}
+	$scope.redirect = function(){
+  		$location.url('/convertvideo.html');
+	}		
 
-      $scope.goToConvert = function(curr_video) {
-      	console.log(curr_video)
-      	$scope.selectedVideo=curr_video
-      	console.log($scope.selectedVideo)
-
-      	//console.log($location.url);
-      	//window.location(""_
-        //$location.absUrl('file:///Users/jamshed/Desktop/HackPrinceton/instatranslate/Angular/convertvideo.html');
+    $scope.goToConvert = function(curr_video) {
+      	console.log(curr_video);
+      	$scope.$parent.selectedVideo=curr_video;
+      	console.log($scope.selectedVideo);
     };
 
-    $scope.checkScope = function() {
+   	$scope.checkScope = function() {
       	console.log($scope.selectedVideo)
     };
 
-    $scope.toggle = function() {
+	$scope.toggle = function() {
       	console.log($scope.show);
       	$scope.$parent.show=!$scope.$parent.show;
       	console.log($scope.show);
-    };
+	};
 
 }]);
